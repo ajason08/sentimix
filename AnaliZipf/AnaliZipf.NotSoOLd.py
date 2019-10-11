10479f27eab082e141525ed3d141cbe5797ff3c6
amnet <amnet04@gmail.com>
2019-10-10 20:57:59 -0500

    codo

---
+++ AnaliZipf/AnaliZipf.NotSoOLd.py
@@ -0,0 +1,358 @@
+import pandas as pd
+import numpy as np
+from scipy.special import digamma
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+
+def harmonic(x):
+    """
+    Returns the armonic number of x
+    If s is complex the result becomes complex.
+    """
+    return digamma(x + 1) + np.euler_gamma
+
+def harmonic_g1(c,n,m):
+    """
+    Return the armonic generalized  number
+    for parameters by a for cicle
+
+    Parameters:
+    c (int): add constant
+    n (int): sumatory count limit
+    m (float): add power
+    """
+    harm = 0
+    for k in range (1,n):
+        oldharm = harm
+        suma = c+k
+        potencia = suma**m
+        cociente = 1/potencia
+        harm += cociente
+        if format(oldharm, '.16g') == format(harm, '.16g'):
+            return harm
+    return harm
+
+def harmonic_g2(c,n,m):
+    return harmonic_g1(0,c+n,m)-harmonic_g1(0,c+1,m)
+
+def euler_aprox(x):
+    e = 0
+    for i in range(0, int(x)):
+        e += 1/np.math.factorial(i)
+    return e
+
+def zipf_graph(zipf, log=True, rank="DenRank"):
+    data = zipf.freq_rank[["Freq",rank]]
+    zipf_model = zipf.zipf
+
+    if log == False:
+        zipf_model = np.exp(zipf_model)
+        info = "Freq vs Rank"
+        x_label = "Rank"
+        zipf_ylabel = "Freq"
+    elif log == "Freq":
+        data["Freq"] = np.log(data["Freq"])
+        info = "ln(Freq) vs Rank"
+        x_label = "Rank"
+        zipf_ylabel = "ln(Freq)"
+    elif log == "Rank":
+        data[list_of_types] = np.log(data[list_of_types])
+        zipf_model = np.exp(zipf_model)
+        print(zipf_model)
+        info = "Freq vs ln(Rank)"
+        x_label = "ln(Rank)"
+        zipf_ylabel = "Freq"
+    else:
+        data = np.log(data)
+        info = "ln(Freq) vs ln(Rank)"
+        x_label = "ln(Rank)"
+        zipf_ylabel = "ln(Freq)"
+
+
+
+
+def zipf_type_of_rank_comparision_graph(zipf,
+                                        log = True,
+                                        list_of_types = ["AvgRank",
+                                                         "DenRank",
+                                                         "MinRank",
+                                                         "MaxRank",
+                                                         "FirRank"],
+                                        comparision_list=["Error", "Types", "Tokens"]):
+    data = zipf.freq_rank
+    zipf_model = zipf.zipf
+    number_of_types = len(list_of_types)
+
+    number_of_comparitions = len(comparision_list)
+
+    if log == False:
+        zipf_model = np.exp(zipf_model)
+        info = "Freq vs Rank"
+        x_label = "Rank"
+        zipf_ylabel = "Freq"
+    elif log == "Freq":
+        data["Freq"] = np.log(data["Freq"])
+        info = "ln(Freq) vs Rank"
+        x_label = "Rank"
+        zipf_ylabel = "ln(Freq)"
+    elif log == "Rank":
+        data[list_of_types] = np.log(data[list_of_types])
+        zipf_model = np.exp(zipf_model)
+        print(zipf_model)
+        info = "Freq vs ln(Rank)"
+        x_label = "ln(Rank)"
+        zipf_ylabel = "Freq"
+    else:
+        data = np.log(data)
+        info = "ln(Freq) vs ln(Rank)"
+        x_label = "ln(Rank)"
+        zipf_ylabel = "ln(Freq)"
+
+    y = data["Freq"]
+
+    y_lables = [zipf_ylabel]
+
+    if "Error" in comparision_list:
+        y_lables.append("Square error")
+    if "Types" in comparision_list:
+        y_lables.append("Types per rank")
+    if "Tokens" in comparision_list:
+        y_lables.append("Tokens per rank")
+
+
+    # Set the graph
+    fig, axs = plt.subplots(number_of_comparitions+1, number_of_types, sharex="col", sharey="row")
+    fig.suptitle("Zipf law in diferents ranks\n"+info)
+
+    for i in range (0, number_of_types):
+        x = data[list_of_types[i]]
+        comp = []
+        zipf_y = zipf_model[list_of_types[i]]
+        axs[0,i].plot(x,y)
+        axs[0,i].plot(x,zipf_y)
+        axs[0,i].set_title("{}".format(list_of_types[i]))
+        if "Error" in comparision_list:
+            error = np.array(zipf_model["SqrEr{}".format(list_of_types[i])])
+            comp.append(error)
+        if "Types" in comparision_list:
+            types = np.array(zipf_model["SqrEr{}".format(list_of_types[i])])
+            comp.append(types)
+        if "Tokens" in comparision_list:
+            tokens =np.array(zipf_model["SqrEr{}".format(list_of_types[i])])
+            comp.append(tokens)
+        for count, element in enumerate(comp,1):
+            axs[count,i].plot(x,element)
+
+    for ax in axs.flat:
+        ax.set(xlabel=x_label)
+
+    for ax, row in zip(axs[:,0], y_lables):
+        ax.set_ylabel(row)
+
+    # Hide x labels and tick labels for top plots and y ticks for right plots.
+    for ax in axs.flat:
+        ax.label_outer()
+
+    plt.show()
+
+
+
+
+
+
+class zipf():
+
+    """
+    Define elements
+    """
+    freq_ranks = pd.DataFrame()
+
+    # Tokens / Types
+    tokens = 0
+    types = 0
+    TokTypRatio = 0
+
+    # ZipfModel 1 (alpha and k calc with sklearn linear regression)
+    zipf_skl_lr = pd.DataFrame()
+    alpha_skl_lr = 0
+    k_skl_lr = 0
+
+    # ZipfModel 2 (alpha and k calculated with Gelbuk-Sidorov method)
+    zipf_gs_lr = pd.DataFrame()
+    alpha_gs_lr = 0
+    k_gs_lr = 0
+
+    # ZipfModel 3 (alpha and k whith maximum likehood estimation)
+    zipf_mle = pd.DataFrame()
+    alpha_mle = 0
+    k_mle = 0
+
+    # ZipfModel 4 (Alfa and beta calculated with sklearn whith trucated data)
+    zipf_skl_lr_tr = pd.dataframe()
+    alpha_skl_lr_tr = 0
+    k_skl_lr_tr = 0
+
+    # PoliZipfModel (Model whith polinomic linear regresion)
+    polizipf = pd.dataframe()
+    polizipf_coef =  []
+
+    # Mandelbroot maximum likehood
+    mandel_mle = pd.DataFrame()
+    mandel_k = 0
+    mandel_alpha = 0
+
+    # My model (Alpha and k caculated by maximum likehood estimation)
+    rod_mle = pd.dataframe()
+    rod_k_mle = 0
+    rod_alpha_mle = 0
+
+    # My model (K by zero cross, alpha whit e and e aprox)
+    rod = pd.dataframe()
+    rod_k_mle = 0
+    rod_alpha = 0
+    rod_sigma = 0
+
+    # Errors
+    Error = pd.dataframe()
+    SqrEr = pd.dataframe()
+
+
+    #subranks_level = {"Avg":0,"Den":0, "Min":0, "Max":, "Fir":0}
+    errors = {}
+
+    def __init__(self, bow):
+        """
+        Takes a BOW of words of a text and build his Zipfian estructure.
+
+        Params:
+        bow: A BOW of text in dict format which each token as keyword
+        """
+
+        # Takes  the BOW dict and transform it into pd_data
+        self.freq_rank["Freq"] =  pd.Series(bow)
+        # Order the frecuencies from high to low
+        self.freq_rank.sort_values(by="Freq", ascending = False)
+        # Dense rank frecuencies from high to low
+        self.freq_rank["AvgRank"] = self.freq_rank["Freq"].rank(method="average", ascending=False)
+        self.freq_rank["DenRank"] = self.freq_rank["Freq"].rank(method="dense", ascending=False)
+        self.freq_rank["MinRank"] = self.freq_rank["Freq"].rank(method="min", ascending=False)
+        self.freq_rank["MaxRank"] = self.freq_rank["Freq"].rank(method="max", ascending=False)
+        self.freq_rank["FirRank"] = self.freq_rank["Freq"].rank(method="first", ascending=False)
+
+        # Minimum values
+        self.min_raw_freq = self.freq_rank["Freq"].min()
+        self.min_avr_rank = self.freq_rank["AvgRank"].min()
+        self.mix_max_rank = self.freq_rank["MaxRank"].min()
+
+        # Maximum values
+        self.max_raw_freq = self.freq_rank["Freq"].max()
+        self.max_avg_rank = self.freq_rank["AvgRank"].max()
+        self.max_den_rank = self.freq_rank["DenRank"].max()
+        self.max_min_rank = self.freq_rank["MinRank"].max()
+        self.max_max_rank = self.freq_rank["MaxRank"].max()
+        self.max_fir_rank = self.freq_rank["FirRank"].max()
+
+        # Sums
+        self.total_tokens = self.freq_rank["Freq"].sum()
+        self.sum_avg_rank = self.freq_rank["AvgRank"].sum()
+        self.sum_den_rank = self.freq_rank["DenRank"].sum()
+        self.sum_min_rank = self.freq_rank["MinRank"].sum()
+        self.sum_max_rank = self.freq_rank["MaxRank"].sum()
+        self.sum_fir_rank = self.freq_rank["FirRank"].sum()
+
+        # count
+        self.total_types = self.freq_rank.shape[0]
+
+        self.zipf_structure()
+
+        self.avg_nest, self.sub_avgrank = self.fractal_str("AvgRank")
+        self.den_nest, self.sub_denrank = self.fractal_str("DenRank")
+        self.min_nest, self.sub_minrank = self.fractal_str("MinRank")
+        self.max_nest, self.sub_maxrank = self.fractal_str("MaxRank")
+        self.fir_nest, self.sub_firrank = self.fractal_str("FirRank")
+
+        self.harm_avg = harmonic(self.avg_nest)
+        self.harm_den = harmonic(self.den_nest)
+        self.harm_min = harmonic(self.min_nest)
+        self.harm_max = harmonic(self.max_nest)
+        self.harm_fir = harmonic(self.fir_nest)
+
+        self.e_avg = euler_aprox(self.avg_nest)
+        self.e_den = euler_aprox(self.den_nest)
+        self.e_min = euler_aprox(self.min_nest)
+        self.e_max = euler_aprox(self.max_nest)
+        self.e_fir = euler_aprox(self.fir_nest)
+
+    def fractal_str(self, rank):
+        method_dict = {"AvgRank":"average","DenRank":"dense","MinRank":"min","MaxRank":"max","FirRank":"first"}
+        element = self.freq_rank[["Freq",rank]]
+        element = element.rename(columns={"Freq": "F0", rank: "R0"})
+        element["F1"] = element.groupby("R0")["R0"].transform("count")
+        element["AF1"] = element.groupby(["R0"])["F0"].transform("sum")
+        element["R1"] = element["F1"].rank(method=method_dict[rank], ascending=False)
+
+        str_level = 1
+        finish = False
+        while finish == False:
+            element = element
+            str_level+=1
+            freq_key = "F{}".format(str_level)
+            rank_key = "R{}".format(str_level)
+            afre_key = "AF{}".format(str_level)
+            prev_freq_key = "F{}".format(str_level-1)
+            prev_rank_key = "R{}".format(str_level-1)
+            prev_afre_key = "AF{}".format(str_level-1)
+            element[freq_key] = element.groupby(prev_rank_key)[prev_rank_key].transform("count")
+            element[afre_key] = element.groupby([prev_rank_key])["F0"].transform("sum")
+            element[rank_key] = element[freq_key].rank(method=method_dict[rank], ascending=False)
+            if element[freq_key].equals(element[prev_freq_key]):
+                finish = True
+
+        return str_level, element
+
+
+    def subrank_stabilization(self):
+
+        """ This ignore the value of the original frequencies, only counts subrank freq"""
+        self.stabilization = self.freq_rank[["Freq","DenRank"]]
+        self.lengths = [len(self.stabilization)]
+        while len(self.stabilization) != 1:
+            self.stabilization = self.stabilization.groupby("DenRank").count()
+            self.stabilization.index.name=""
+            self.stabilization["DenRank"] = self.stabilization["Freq"].rank(method="dense", ascending=False)
+            self.lengths.append(len(self.stabilization))
+            """print(self.stabilization)
+            print(self.lengths)"""
+        self.n = len(self.lengths)-2
+        self.e_aprox_by_subrankstabilization = 0
+        for x in range (0, self.n):
+            self.e_aprox_by_subrankstabilization += 1/np.math.factorial(x)
+
+
+
+    def zipf_structure(self):
+        self.zipf = np.log(self.freq_rank)
+        for rank in ["AvgRank", "DenRank", "MinRank", "MaxRank", "FirRank"]:
+            base_df = self.freq_rank[["Freq",rank]].groupby(rank).first()
+            x = np.log(np.array(base_df.index)).reshape(-1,1)
+            y = np.log(np.array(base_df["Freq"])).reshape(-1,1)
+            regressor = LinearRegression()
+            regressor.fit(x, y)
+
+
+            self.zipf_k[rank] = regressor.intercept_[0]
+            self.zipf_alpha[rank] = regressor.coef_[0][0]
+
+
+            self.zipf[rank] = self.zipf_alpha[rank]*self.zipf[rank]+self.zipf_k[rank]
+            self.zipf["AbsEr"+rank] = abs(self.zipf[rank] - self.zipf["Freq"])
+            self.zipf["SqrEr"+rank] = (self.zipf[rank] - self.zipf["Freq"])**2
+
+            self.errors["ZMeanAE"+rank] = self.zipf["AbsEr"+rank].mean()
+            self.errors["ZMeanSE"+rank] = self.zipf["SqrEr"+rank].mean()
+            self.errors["ZMeanRSE"+rank] = (self.zipf["SqrEr"+rank].mean())**(1/2)
--- AnaliZipf/AnaliZipf.py
+++ AnaliZipf/AnaliZipf.py
@@ -47,101 +47,99 @@ def euler_aprox(x):
     return e
 
 
-def zipf_type_of_rank_comparision_graph(zipf,
-                                        log = True,
-                                        list_of_types = ["AvgRank",
-                                                         "DenRank",
-                                                         "MinRank",
-                                                         "MaxRank",
-                                                         "FirRank"],
-                                        comparision_list=["Error", "Types", "Tokens"]):
-    data = zipf.freq_rank
-    zipf_model = zipf.zipf
-    number_of_types = len(list_of_types)
-
-    number_of_comparitions = len(comparision_list)
-
-    if log == False:
-        zipf_model = np.exp(zipf_model)
-        info = "Freq vs Rank"
-        x_label = "Rank"
-        zipf_ylabel = "Freq"
-    elif log == "Freq":
-        data["Freq"] = np.log(data["Freq"])
-        info = "ln(Freq) vs Rank"
-        x_label = "Rank"
-        zipf_ylabel = "ln(Freq)"
-    elif log == "Rank":
-        data[list_of_types] = np.log(data[list_of_types])
-        zipf_model = np.exp(zipf_model)
-        print(zipf_model)
-        info = "Freq vs ln(Rank)"
-        x_label = "ln(Rank)"
-        zipf_ylabel = "Freq"
-    else:
-        data = np.log(data)
-        info = "ln(Freq) vs ln(Rank)"
-        x_label = "ln(Rank)"
-        zipf_ylabel = "ln(Freq)"
-
-    y = data["Freq"]
-
-    y_lables = [zipf_ylabel]
-
-    if "Error" in comparision_list:
-        y_lables.append("Square error")
-    if "Types" in comparision_list:
-        y_lables.append("Types per rank")
-    if "Tokens" in comparision_list:
-        y_lables.append("Tokens per rank")
-
-
-    # Set the graph
-    fig, axs = plt.subplots(number_of_comparitions+1, number_of_types, sharex="col", sharey="row")
-    fig.suptitle("Zipf law in diferents ranks\n"+info)
-
-    for i in range (0, number_of_types):
-        x = data[list_of_types[i]]
-        comp = []
-        zipf_y = zipf_model[list_of_types[i]]
-        axs[0,i].plot(x,y)
-        axs[0,i].plot(x,zipf_y)
-        axs[0,i].set_title("{}".format(list_of_types[i]))
-        if "Error" in comparision_list:
-            error = np.array(zipf_model["SqrEr{}".format(list_of_types[i])])
-            comp.append(error)
-        if "Types" in comparision_list:
-            types = np.array(zipf_model["SqrEr{}".format(list_of_types[i])])
-            comp.append(types)
-        if "Tokens" in comparision_list:
-            tokens =np.array(zipf_model["SqrEr{}".format(list_of_types[i])])
-            comp.append(tokens)
-        for count, element in enumerate(comp,1):
-            axs[count,i].plot(x,element)
-
-    for ax in axs.flat:
-        ax.set(xlabel=x_label)
-
-    for ax, row in zip(axs[:,0], y_lables):
-        ax.set_ylabel(row)
-
-    # Hide x labels and tick labels for top plots and y ticks for right plots.
-    for ax in axs.flat:
-        ax.label_outer()
-
-    plt.show()
-
-
-
-
-
 
 class zipf():
+
+    """
+    Define elements
+    """
     freq_rank = pd.DataFrame()
-    zipf_k = {}
-    zipf_alpha = {}
-    #subranks_level = {"Avg":0,"Den":0, "Min":0, "Max":, "Fir":0}
-    errors = {}
+
+    # Tokens / Types
+    token_count = 0
+    types_count = 0
+    TypTokRatio = 0
+
+    # ZipfModel Cero cross (To calculate data truncation)
+    zipf_cero_cross = pd.DataFrame()
+    alpha_cero_cross = {}
+    k_cero_cross = {}
+    cero_trunkated = pd.DataFrame()
+
+    # ZipfModel 1 (alpha and k calc with sklearn linear regression)
+    zipf_skl_lr = pd.DataFrame()
+    alpha_skl_lr = {}
+    k_skl_lr = {}
+
+    # ZipfModel 2 (alpha and k calculated with Gelbuk-Sidorov method)
+    zipf_gs_lr = pd.DataFrame()
+    alpha_gs_lr = {}
+    k_gs_lr = {}
+
+    # ZipfModel 3 (alpha and k whith maximum likehood estimation)
+    zipf_mle = pd.DataFrame()
+    alpha_mle = {}
+    k_mle = {}
+
+    # ZipfModel 4 (Alfa and beta calculated with sklearn whith trucated data)
+    zipf_skl_lr_tr = pd.DataFrame()
+    alpha_skl_lr_tr = {}
+    k_skl_lr_tr = {}
+
+    # PoliZipfModel (Model whith polinomic linear regresion)
+    polizipf = pd.DataFrame()
+    polizipf_coef =  {}
+
+    # Mandelbroot maximum likehood
+    mandel_mle = pd.DataFrame()
+    mandel_k = {}
+    mandel_alpha = {}
+
+    # My model (Alpha and k caculated by maximum likehood estimation)
+    rod_mle = pd.DataFrame()
+    rod_k_mle = 0
+    rod_alpha_mle = 0
+
+    # My model (K by zero cross, alpha whit e and e aprox)
+    rod = pd.DataFrame()
+    rod_k_mle = {}
+    rod_alpha = {}
+    rod_sigma = {}
+
+    # Errors
+    Error = pd.DataFrame()
+    SqrEr = pd.DataFrame()
+
+    # kness
+    knees = {"AvgRank":pd.DataFrame(),
+             "DenRank":pd.DataFrame(),
+             "MinRank":pd.DataFrame(),
+             "MaxRank":pd.DataFrame(),
+             "FirRank":pd.DataFrame()}
+    max_knees = {"AvgRank":[],
+             "DenRank":[],
+             "MinRank":[],
+             "MaxRank":[],
+             "FirRank":[]}
+
+    # Fractal structures and correspondig harmonics and e aproximations
+    avg_nest, sub_avgrank = 0, pd.DataFrame()
+    den_nest, sub_denrank = 0, pd.DataFrame()
+    min_nest, sub_minrank = 0, pd.DataFrame()
+    max_nest, sub_maxrank = 0, pd.DataFrame()
+    fir_nest, sub_firrank = 0, pd.DataFrame()
+
+    harm_avg = 0
+    harm_den = 0
+    harm_min = 0
+    harm_max = 0
+    harm_fir = 0
+
+    e_avg = 0
+    e_den = 0
+    e_min = 0
+    e_max = 0
+    e_fir = 0
 
     def __init__(self, bow):
         """
@@ -153,6 +151,24 @@ class zipf():
 
         # Takes  the BOW dict and transform it into pd_data
         self.freq_rank["Freq"] =  pd.Series(bow)
+
+        # Order and calculate ranks
+        self.Rank()
+
+        # Types and token
+        self.token_count = self.freq_rank["Freq"].sum()
+        self.types_count = self.freq_rank.shape[0]
+        self.TypTokRatio = self.types_count/self.token_count
+
+        # Set zerocross line
+        self.ZipfZeroCross()
+
+        # Set knees
+        self.Knee()
+
+
+
+    def Rank(self):
         # Order the frecuencies from high to low
         self.freq_rank.sort_values(by="Freq", ascending = False)
         # Dense rank frecuencies from high to low
@@ -162,115 +178,28 @@ class zipf():
         self.freq_rank["MaxRank"] = self.freq_rank["Freq"].rank(method="max", ascending=False)
         self.freq_rank["FirRank"] = self.freq_rank["Freq"].rank(method="first", ascending=False)
 
-        # Minimum values
-        self.min_raw_freq = self.freq_rank["Freq"].min()
-        self.min_avr_rank = self.freq_rank["AvgRank"].min()
-        self.mix_max_rank = self.freq_rank["MaxRank"].min()
-
-        # Maximum values
-        self.max_raw_freq = self.freq_rank["Freq"].max()
-        self.max_avg_rank = self.freq_rank["AvgRank"].max()
-        self.max_den_rank = self.freq_rank["DenRank"].max()
-        self.max_min_rank = self.freq_rank["MinRank"].max()
-        self.max_max_rank = self.freq_rank["MaxRank"].max()
-        self.max_fir_rank = self.freq_rank["FirRank"].max()
-
-        # Sums
-        self.total_tokens = self.freq_rank["Freq"].sum()
-        self.sum_avg_rank = self.freq_rank["AvgRank"].sum()
-        self.sum_den_rank = self.freq_rank["DenRank"].sum()
-        self.sum_min_rank = self.freq_rank["MinRank"].sum()
-        self.sum_max_rank = self.freq_rank["MaxRank"].sum()
-        self.sum_fir_rank = self.freq_rank["FirRank"].sum()
-
-        # count
-        self.total_types = self.freq_rank.shape[0]
-
-        self.zipf_structure()
-
-        self.avg_nest, self.sub_avgrank = self.fractal_str("AvgRank")
-        self.den_nest, self.sub_denrank = self.fractal_str("DenRank")
-        self.min_nest, self.sub_minrank = self.fractal_str("MinRank")
-        self.max_nest, self.sub_maxrank = self.fractal_str("MaxRank")
-        self.fir_nest, self.sub_firrank = self.fractal_str("FirRank")
-
-        self.harm_avg = harmonic(self.avg_nest)
-        self.harm_den = harmonic(self.den_nest)
-        self.harm_min = harmonic(self.min_nest)
-        self.harm_max = harmonic(self.max_nest)
-        self.harm_fir = harmonic(self.fir_nest)
-
-        self.e_avg = euler_aprox(self.avg_nest)
-        self.e_den = euler_aprox(self.den_nest)
-        self.e_min = euler_aprox(self.min_nest)
-        self.e_max = euler_aprox(self.max_nest)
-        self.e_fir = euler_aprox(self.fir_nest)
-
-    def fractal_str(self, rank):
-        method_dict = {"AvgRank":"average","DenRank":"dense","MinRank":"min","MaxRank":"max","FirRank":"first"}
-        element = self.freq_rank[["Freq",rank]]
-        element = element.rename(columns={"Freq": "F0", rank: "R0"})
-        element["F1"] = element.groupby("R0")["R0"].transform("count")
-        element["AF1"] = element.groupby(["R0"])["F0"].transform("sum")
-        element["R1"] = element["F1"].rank(method=method_dict[rank], ascending=False)
-
-        str_level = 1
-        finish = False
-        while finish == False:
-            element = element
-            str_level+=1
-            freq_key = "F{}".format(str_level)
-            rank_key = "R{}".format(str_level)
-            afre_key = "AF{}".format(str_level)
-            prev_freq_key = "F{}".format(str_level-1)
-            prev_rank_key = "R{}".format(str_level-1)
-            prev_afre_key = "AF{}".format(str_level-1)
-            element[freq_key] = element.groupby(prev_rank_key)[prev_rank_key].transform("count")
-            element[afre_key] = element.groupby([prev_rank_key])["F0"].transform("sum")
-            element[rank_key] = element[freq_key].rank(method=method_dict[rank], ascending=False)
-            if element[freq_key].equals(element[prev_freq_key]):
-                finish = True
-
-        return str_level, element
-
-
-    def subrank_stabilization(self):
-
-        """ This ignore the value of the original frequencies, only counts subrank freq"""
-        self.stabilization = self.freq_rank[["Freq","DenRank"]]
-        self.lengths = [len(self.stabilization)]
-        while len(self.stabilization) != 1:
-            self.stabilization = self.stabilization.groupby("DenRank").count()
-            self.stabilization.index.name=""
-            self.stabilization["DenRank"] = self.stabilization["Freq"].rank(method="dense", ascending=False)
-            self.lengths.append(len(self.stabilization))
-            """print(self.stabilization)
-            print(self.lengths)"""
-        self.n = len(self.lengths)-2
-        self.e_aprox_by_subrankstabilization = 0
-        for x in range (0, self.n):
-            self.e_aprox_by_subrankstabilization += 1/np.math.factorial(x)
-
-
-
-    def zipf_structure(self):
-        self.zipf = np.log(self.freq_rank)
+    def ZipfZeroCross(self):
+        self.k_cero_cross = np.log(self.freq_rank["Freq"].max())
+        k = self.k_cero_cross
         for rank in ["AvgRank", "DenRank", "MinRank", "MaxRank", "FirRank"]:
-            base_df = self.freq_rank[["Freq",rank]].groupby(rank).first()
-            x = np.log(np.array(base_df.index)).reshape(-1,1)
-            y = np.log(np.array(base_df["Freq"])).reshape(-1,1)
-            regressor = LinearRegression()
-            regressor.fit(x, y)
+            self.alpha_cero_cross = self.k_cero_cross/np.log(self.freq_rank[rank].max())
+            alpha = self.alpha_cero_cross
+            self.zipf_cero_cross[rank] = -alpha*np.log(self.freq_rank[rank])+k
+            self.Error["ZZC".format(rank)] = np.log(self.freq_rank["Freq"])-self.zipf_cero_cross[rank]
+            self.SqrEr["ZZC".format(rank)] = (np.log(self.freq_rank["Freq"])-self.zipf_cero_cross[rank])**2
 
-
-            self.zipf_k[rank] = regressor.intercept_[0]
-            self.zipf_alpha[rank] = regressor.coef_[0][0]
-
-
-            self.zipf[rank] = self.zipf_alpha[rank]*self.zipf[rank]+self.zipf_k[rank]
-            self.zipf["AbsEr"+rank] = abs(self.zipf[rank] - self.zipf["Freq"])
-            self.zipf["SqrEr"+rank] = (self.zipf[rank] - self.zipf["Freq"])**2
-
-            self.errors["ZMeanAE"+rank] = self.zipf["AbsEr"+rank].mean()
-            self.errors["ZMeanSE"+rank] = self.zipf["SqrEr"+rank].mean()
-            self.errors["ZMeanRSE"+rank] = (self.zipf["SqrEr"+rank].mean())**(1/2)
+    def Knee(self):
+        base = np.log(self.freq_rank)
+        for rank in ["AvgRank", "DenRank", "MinRank", "MaxRank", "FirRank"]:
+            base_rank = base[["Freq",rank]].groupby(rank).first()
+            x = np.array(list(base_rank.index))
+            y = np.array(base_rank["Freq"])
+            p1 = np.array([x[0], y[0]])
+            p2 = np.array([x[-1], y[-1]])
+            self.knees[rank] = base_rank
+            for i in range (0, len(x)-1):
+                p3 = np.array([x[i],y[i]])
+                print(p1,p2,p3)
+                distance = np.linalg.norm(np.cross(p2-p1, p1-p3))/np.linalg.norm(p2-p1)
+                self.knees[rank]["Freq"].iloc[i] = distance
+            self.max_knees[rank] = self.knees[rank]["Freq"].max()
